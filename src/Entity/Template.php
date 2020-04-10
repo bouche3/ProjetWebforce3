a14fbaa7135d3cb5f3c2ea1aaa2c0211af41d9d8
Stephane Albertini <bouche3@msn.com>
2020-04-10 20:50:37 +0200

    gestion des membres

---
+++ src/Entity/Template.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TemplateRepository")
+ */
+class Template
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $templateName;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Article", mappedBy="nameTemplate")
+     */
+    private $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTemplateName(): ?string
+    {
+        return $this->templateName;
+    }
+
+    public function setTemplateName(string $templateName): self
+    {
+        $this->templateName = $templateName;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->setNameTemplate($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->contains($article)) {
+            $this->articles->removeElement($article);
+            // set the owning side to null (unless already changed)
+            if ($article->getNameTemplate() === $this) {
+                $article->setNameTemplate(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Form/TemplateType.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Country;
+use App\Entity\ImageTemplate;
+use App\Entity\MixteTemplate;
+use App\Entity\Template;
+use App\Entity\TextTemplate;
+use App\Entity\TravelCategory;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TemplateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'city',
+                TextType::class,
+                [
+                    'label'=>'La ville de votre voyage'
+                ]
+            )
+            ->add(
+                'title',
+                TextType::class,
+                [
+                    'label'=>'Le titre de votre article'
+                ]
+            )
+            ->add(
+                'countryid',
+                EntityType::class,
+                [
+                    'label'=>'Le pays de votre voyage',
+                    'class'=>Country::class,
+                    'choice_label'=>'country_name',
+                    'placeholder'=>'Choisissez un pays'
+                ]
+            )
+            ->add(
+                'categoryid',
+                EntityType::class,
+                [
+                    'label'=>'Le type de voyage',
+                    'class'=>TravelCategory::class,
+                    'choice_label'=>'category_name',
+                    'placeholder'=>'Choisissez le type de voyage'
+                ]
+            )
+            ->add(
+                'nameTemplate',
+                EntityType::class,
+                [
+                    'label'=>'La mise en page',
+                    'class'=>Template::class,
+                    'choice_label'=>'template_name',
+                    'placeholder'=>'Choisissez votre mise en page'
+
+                ]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
